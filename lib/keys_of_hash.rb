649e273036e55768546210ed13fe4f4cbb2638c2
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-06 18:55:36 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,3 +3,17 @@ class Hash
     map {|key, value| args.include?(value) ? key : nil }.compact
   end
 end
+
+
+class Hash
+  def keys_of(*arguments) #add any arguments
+    array = [] #create a new array for the result
+    #self: arguments
+    self.each do |key, value|
+      if arguments.include?(value) #check match value from arguments
+        array << key
+      end
+    end
+    array
+  end
+end
