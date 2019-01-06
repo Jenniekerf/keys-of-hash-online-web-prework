7cfadd1ffccea081d04b9fe01622865ba8e4649f
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-06 19:07:34 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,10 @@
 class Hash
   def keys_of(*args) 
     array = [] 
-    self.each do |key, value|
+    self.each {|key, value|
       if args.include?(value) 
         array << key
-      end
+      }
     end
     array
   end
